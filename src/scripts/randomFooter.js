e85f0321f41a9b09e1f10610aa9383fc00b54f89
Abdou990F <benari102@gmail.com>
2023-09-07 23:16:28 +0100

    Cool Update

--- src/scripts/randomFooter.js
+++ src/scripts/randomFooter.js
@@ -6,7 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
       "Omegle moment",
       "Hold it, I just got a bruh moment",
       "BensariSMP was a cool Minecraft server of mine",
-      "JavaScript? Never heard of it... I think"
+      "JavaScript? Never heard of it... I think",
+      "Python is cool",
+      "Godot is awesome",
+      "Pssss... My Github Username: \"AbdooOwd\"",
+      "I'm a smart student, but a dumb person",
+      "Algeria, the knock-offs counrty",
+      "Algeria is cool, but LAW isn't applied",
+      "More random text!"
     ];
     const randomIndex = Math.floor(Math.random() * randomChoices.length);
     pElement.textContent = randomChoices[randomIndex];

d267ca79318de2ca47a0d17194e16d37f056a501
Abdou990F <benari102@gmail.com>
2023-10-12 20:27:48 +0100

    Cool

--- src/components/fun/fun.js
+++ src/components/fun/fun.js
@@ -1,15 +1,38 @@
+const head = document.getElementById("leHead");
+
+// MipMip Section
+
 const mipmip = document.getElementById("mip");
 
+let ded = false;
 mipmip.addEventListener('click', function() {
-    let ded = false;
-    let n = 0;
 
-    while (ded == false) {
-        if (mipmip.style.marginLeft == "600px") {
-            ded = true;
-        }
-        mipmip.style.marginLeft = n + "px";
-        n += 1;
+    if (ded == false) {
+        mipmip.style.marginLeft = (innerWidth / 2) + "px";
+        ded = true;
+    } else {
+        mipmip.style.marginLeft = "0px";
+        ded = false;
     }
     
+    
+});
+
+
+// Barrel Roll
+
+const rick = document.getElementById("rick-roll");
+
+let rolled = false;
+let rotation = 0;
+rick.addEventListener("click", function() {
+    rotation += 180;
+    let rot_str = "rotate(" + rotation + "deg)";
+    if (rolled == false) {
+        head.style.transform = rot_str;
+        rolled = true;
+    } else {
+        head.style.transform = rot_str;
+        rolled = false;
+    }
 });
